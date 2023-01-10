e2a9fa9043de7f77816f52617303b6723f5143e0
marcosccoutinho <marcao_hhc41@hotmail.com>
2023-01-10 10:01:04 -0300

    atualizaçao

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var alt=(document.getElementById("altura").value)/100;
 var pes=document.getElementById("peso").value;
 var imc = pes / alt**2;
 var res=""
+var figura=""
 if (imc <18.5){
-   res= "voce esta magrelo"
+   res= "voce esta magro segue "
 }else if (imc<24.9){
     res= "parabens voce esta em forma "
 }else if (imc<29.9){
@@ -14,5 +15,6 @@ if (imc <18.5){
 }else if (imc>39.9){
     res = "voce esta com obesidade morbida"
 }
-document.getElementById("resultado").innerHTML= res
+document.getElementById("resultado").innerHTML=res
+
 }
